e22f44c67b66e0f249ca3be68c4601247e3e5558
Jianfeng <ginfungchan@gmail.com>
2015-11-03 23:29:28 -0500

    movie ground quick finder

---
+++ MovieFinder.py
@@ -0,0 +1,26 @@
+import random,pdb
+
+class MovieFinder(object):
+    def __init__(self):
+        f = open('movie_ground.txt')
+        self.lines = f.readlines()
+        self.N = len(self.lines)
+        f.close()
+
+    def _readline(self, line):
+        p = self.lines[line].split(' ')
+        return int(p[0]), float(p[1])
+
+    def get_movie_ground(self, movie_id):
+        #binary search
+        left = 0
+        right = self.N
+        while left <= right:
+            i,ground = self._readline(int(left+right)/2)
+            if i == movie_id: return ground
+            if i < movie_id: left = i
+            else: right = i
+        return None
+
+finder = MovieFinder()
+print finder.get_movie_ground(70)
